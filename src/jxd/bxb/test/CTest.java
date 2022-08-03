1fb6069bf22f5c27c711f5affcdf453aca47b16f
白新报 <3088467406@qq.com>
2022-08-03 16:35:50 +0800

    C#

--- src/jxd/bxb/test/CTest.java
+++ src/jxd/bxb/test/CTest.java
@@ -1,6 +1,7 @@
 package jxd.bxb.test;
 
 import jxd.bxb.test.Connect.Conn.DbConnect;
+import jxd.bxb.test.utils.StringUtil;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
@@ -14,7 +15,7 @@ public class CTest {
 
     @Test
     public void model() throws Exception {
-        String tableName = "T_PROCESS_INTERLOCK_ACCOUNT";
+        String tableName = "T_PROCESS_CARD_ACCOUNT";
         List<String> fieldList = DbConnect.getFieldList(tableName);
         List<String> fieldTypeList = DbConnect.getFieldTypeList(tableName);
         List<String> fieldDescList = DbConnect.getFieldDescList(tableName);
@@ -25,28 +26,28 @@ public class CTest {
                     "/// " + fieldDescList.get(i)+ "\n" +
                     "/// </summary>");
             if (fieldTypeList.get(i).indexOf("int") > -1) {
-                System.out.println(start + "int " + fieldList.get(i) + getAndSet);
+                System.out.println(start + "int " + StringUtil.strToStr(fieldList.get(i)) + getAndSet);
             } else {
-                System.out.println(start + "String " + fieldList.get(i) + getAndSet);
+                System.out.println(start + "String " + StringUtil.strToStr(fieldList.get(i)) + getAndSet);
             }
         }
     }
 
     @Test
     public void xml() throws SQLException {
-        String tableName = "T_PROCESS_INTERLOCK_ACCOUNT";
+        String tableName = "T_PROCESS_CARD_ACCOUNT";
         List<String> fieldList = DbConnect.getFieldList(tableName);
         List<String> fieldTypeList = DbConnect.getFieldTypeList(tableName);
         List<String> fieldDescList = DbConnect.getFieldDescList(tableName);
         for (int i = 0; i < fieldList.size(); i++) {
             System.out.println("<!--" + fieldDescList.get(i) + "-->");
             if (i == 0) {
-                System.out.println("<id name=\"" + fieldList.get(i) + "\" type=\"string\">");
+                System.out.println("<id name=\"" + StringUtil.strToStr(fieldList.get(i)) + "\" type=\"string\">");
                 System.out.println("<column name=\"" + fieldList.get(i) + "\" length=\"32\" not-null=\"true\"/>");
                 System.out.println("<generator class=\"native\" />");
                 System.out.println("</id>");
             } else {
-                System.out.println("<property name=\"" + fieldList.get(i) + "\" type=\"string\">");
+                System.out.println("<property name=\"" + StringUtil.strToStr(fieldList.get(i)) + "\" type=\"string\">");
                 System.out.println("<column name=\"" + fieldList.get(i) + "\" length=\"100\" not-null=\"false\"/>");
                 System.out.println("</property>");
             }
